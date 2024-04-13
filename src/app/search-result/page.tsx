89407cd9389799db3cda39f3261c682d32f4f775
Bhavesh Dewangan <ybkdddd@gmail.com>
2024-04-14 03:47:38 +0530

    Revert "- Add suspense boundary to search result page"
    This reverts commit bdc79a4911886b0b14177ea031145f70436abdae.

--- src/app/search-result/page.tsx
+++ src/app/search-result/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ICsvData } from "@/model/data";
 import Image from "next/image";
 
@@ -52,35 +52,33 @@ const SearchResultPage = () => {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex flex-col text-white rounded-lg gap-4 justify-center">
-        <h1 className="text-2xl font-bold mb-4">Search Results</h1>
-        {searchResults?.map((result, index) => (
-          <div
-            key={index}
-            className="bg-black text-white p-4 rounded-lg shadow-md"
-          >
-            <p className="font-bold mb-2">Brand Name: {result["BRAND NAME"]}</p>
-            <p className="mb-2">Product Name: {result["PRODUCT NAME"]}</p>
-            <p className="mb-2">Category: {result.CATEGORY}</p>
-            <p className="mb-2">Subcategory: {result.SUBCATEGORY}</p>
-            <p className="mb-2">Ingredients: {result.INGREDIENTS}</p>
-            <p className={`mb-2 ${getNovaGroupColor(result["NOVA Group"])}`}>
-              Nova Group: {result["NOVA Group"]}
-            </p>
-            <div className="flex justify-center">
-              <Image
-                src={result["Image Links"]}
-                alt="product"
-                width={150}
-                height={150}
-                className="rounded-lg"
-              />
-            </div>
+    <div className="flex flex-col text-white rounded-lg gap-4 justify-center">
+      <h1 className="text-2xl font-bold mb-4">Search Results</h1>
+      {searchResults?.map((result, index) => (
+        <div
+          key={index}
+          className="bg-black text-white p-4 rounded-lg shadow-md"
+        >
+          <p className="font-bold mb-2">Brand Name: {result["BRAND NAME"]}</p>
+          <p className="mb-2">Product Name: {result["PRODUCT NAME"]}</p>
+          <p className="mb-2">Category: {result.CATEGORY}</p>
+          <p className="mb-2">Subcategory: {result.SUBCATEGORY}</p>
+          <p className="mb-2">Ingredients: {result.INGREDIENTS}</p>
+          <p className={`mb-2 ${getNovaGroupColor(result["NOVA Group"])}`}>
+            Nova Group: {result["NOVA Group"]}
+          </p>
+          <div className="flex justify-center">
+            <Image
+              src={result["Image Links"]}
+              alt="product"
+              width={150}
+              height={150}
+              className="rounded-lg"
+            />
           </div>
-        ))}
-      </div>
-    </Suspense>
+        </div>
+      ))}
+    </div>
   );
 };
 
