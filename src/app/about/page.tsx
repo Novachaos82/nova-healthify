025da80c1d30baa999f542c3b99260e453ec3d61
Novachaos82 <ybkdddd@gmail.com>
2024-04-13 17:50:02 +0530

    - made about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,8 @@
 import InfoCards from "@/components/aboutus/InfoCards";
+import Image from "next/image";
 import { FC } from "react";
+import WhyUs from "@/assets/aboutus/WhyUs.png";
+import humanBrain from "@/assets/aboutus/humanBrain.png";
 
 interface pageProps {}
 
@@ -23,15 +26,47 @@ const page: FC<pageProps> = ({}) => {
           </p>
         </div>
       </div>
-      <div className="flex flex-col max-w-[1200px]  mt-20 mx-auto mb-20">
+      <div className="flex flex-col max-w-[1200px]  mt-20 mx-auto mb-20 gap-6">
         <div className="flex justify-between">
           <InfoCards />
           <InfoCards />
         </div>
-        <div>Why us?</div>
+        <div className="flex justify-center">
+          <Image src={WhyUs} alt="whyus" />
+        </div>
         <div className="flex justify-between">
           <InfoCards />
           <InfoCards />
+        </div>
+      </div>
+      {/* mission card */}
+      <div className="max-w-[1200px] flex shadow-md rounded-[24px] mx-auto bg-white mb-20 p-10">
+        <div className="w-[60%] flex flex-col justify-center gap-4">
+          <h1 className="text-[24px]"> Our Mission:</h1>
+          <p className="text-[16px]">
+            Nova Healthify is on a mission to revolutionize food classification.
+            We strive to bring accuracy and efficiency to the forefront, making
+            it easier for enthusiasts, chefs, and food lovers to explore and
+            understand the diverse world of cuisines.
+          </p>
+        </div>
+        <div className="w-[40%] flex flex-col items-end">
+          <Image src={humanBrain} alt="humanBrain" />
+        </div>
+      </div>
+
+      <div className="max-w-[1200px] mx-auto">
+        <div className="max-w-[70%] flex shadow-md rounded-[24px]  bg-white mb-20 p-10">
+          <div className=" flex flex-col justify-center gap-4">
+            <h1 className="text-[24px]"> What Sets Us Apart</h1>
+            <p className="text-[16px]">
+              Our commitment lies in the precision of our machine learning
+              algorithms. We continuously adapt and evolve, ensuring that the
+              classifications are not just accurate but tailored to your
+              preferences. Nova Healthify is more than a platform; its your
+              companion in unraveling the nuances of taste.
+            </p>
+          </div>
         </div>
       </div>
     </div>
